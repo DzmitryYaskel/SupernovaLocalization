b19f1aef1e81a9770f08049c910f9afd65d30f27
Dmitry Yaskel <dzmitry.yaskel@dpmapps.com>
2025-04-26 13:54:09 +0300

    Initial commit. v0.0.1

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SupernovaLocalization",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "SupernovaLocalization",
+            targets: ["SupernovaLocalization"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "SupernovaLocalization",
+            resources: [
+                .process("Localizations")
+            ]
+        ),
+        .testTarget(
+            name: "SupernovaLocalizationTests",
+            dependencies: ["SupernovaLocalization"]),
+    ]
+)
