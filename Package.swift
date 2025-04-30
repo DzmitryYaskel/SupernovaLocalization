2be2d46a898a6236b83f40d123d1ef7548fdca36
Dmitry Yaskel <dzmitry.yaskel@dpmapps.com>
2025-05-01 00:26:54 +0300

    v0.0.4

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     targets: [
         .target(
             name: "SupernovaLocalization",
+            path: "Sources",
             resources: [
                 .process("Resources")
             ]
